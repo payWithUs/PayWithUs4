2115074c946429085cc5fcb2c4ce44876b3cbbce
payWithUs <ajar16677@gmail.com>
2023-11-01 15:09:14 +0200

    test1

--- App.js
+++ App.js
@@ -1,20 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Keyboard } from "react-native";
+import { Container, StyledTextInput, StyledButton } from "./src/logIn.style";
+import { initializeApp } from 'firebase/app';
+
+
+// Initialize Firebase
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC4Nsw1uwW8tNtdxPkJFj4TXNgyXJ8gFec",
+  authDomain: "paytry2-c5399.firebaseapp.com",
+  projectId: "paytry2-c5399",
+  storageBucket: "paytry2-c5399.appspot.com",
+  messagingSenderId: "304588176862",
+  appId: "1:304588176862:web:7c21d6a3142476f645314f"
+};
+
+const app = initializeApp(firebaseConfig);
 
 export default function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {
+    // Handle login logic here, for example, send the email and password to an API
+    console.log("Email:", email);
+    console.log("Password:", password);
+
+    // Clear email and password states
+    setEmail("");
+    setPassword("");
+
+    // Dismiss the keyboard
+    Keyboard.dismiss();
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <>
+      <Container>
+        <StyledTextInput
+          placeholder="Email"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+        />
+        <StyledTextInput
+          placeholder="Password"
+          secureTextEntry
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+        />
+        <StyledButton title="Login" onPress={handleLogin} />
+      </Container>
       <StatusBar style="auto" />
-    </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/logIn.style.js
@@ -0,0 +1,22 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  padding: 16px;
+`;
+
+export const StyledTextInput = styled.TextInput`
+  height: 40px;
+  width: 100%;
+  margin-bottom: 20px;
+  padding: 10px;
+  border-width: 1px;
+  border-color: #ccc;
+  border-radius: 5px;
+`;
+
+export const StyledButton = styled.Button`
+  width: 100%;
+`;
