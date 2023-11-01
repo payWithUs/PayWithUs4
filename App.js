5ef0928e25108a50d1778a8ea870a721d8a46d39
payWithUs <ajar16677@gmail.com>
2023-11-01 15:25:54 +0200

    register

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Keyboard } from "react-native";
+import { StyleSheet, View, Keyboard } from "react-native";
 import { Container, StyledTextInput, StyledButton } from "./src/logIn.style";
-import { initializeApp } from 'firebase/app';
-
+import { initializeApp } from "firebase/app";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 // Initialize Firebase
 // Your web app's Firebase configuration
@@ -13,7 +13,7 @@ const firebaseConfig = {
   projectId: "paytry2-c5399",
   storageBucket: "paytry2-c5399.appspot.com",
   messagingSenderId: "304588176862",
-  appId: "1:304588176862:web:7c21d6a3142476f645314f"
+  appId: "1:304588176862:web:7c21d6a3142476f645314f",
 };
 
 const app = initializeApp(firebaseConfig);
@@ -22,17 +22,24 @@ export default function App() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleLogin = () => {
-    // Handle login logic here, for example, send the email and password to an API
-    console.log("Email:", email);
-    console.log("Password:", password);
+  const handleRegister = async () => {
+    try {
+      const auth = getAuth(app);
+      await createUserWithEmailAndPassword(auth, email, password);
 
-    // Clear email and password states
-    setEmail("");
-    setPassword("");
+      // If the user registration is successful, you can redirect the user to another screen or perform other actions.
+      console.log("User successfully registered!");
 
-    // Dismiss the keyboard
-    Keyboard.dismiss();
+      // Clear email and password states
+      setEmail("");
+      setPassword("");
+
+      // Dismiss the keyboard
+      Keyboard.dismiss();
+    } catch (error) {
+      // Handle registration errors, such as weak password or existing email address
+      console.error("Error registering user: ", error.message);
+    }
   };
 
   return (
@@ -49,7 +56,7 @@ export default function App() {
           value={password}
           onChangeText={(text) => setPassword(text)}
         />
-        <StyledButton title="Login" onPress={handleLogin} />
+        <StyledButton title="Register" onPress={handleRegister} />
       </Container>
       <StatusBar style="auto" />
     </>
